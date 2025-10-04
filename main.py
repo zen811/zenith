eeeb21b4a96993acab518138a7e2d70fe32bb06e
zen811 <khatriharshit84@gmail.com>
2025-10-05 00:06:01 +0530

    initialisation

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+
  
-from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
@@ -10,3 +10,6 @@ from sklearn.metrics import accuracy_score
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
+data = pd.read_csv(".\engine data.csv")
+print(data)
