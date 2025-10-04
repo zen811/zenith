7341552923112100014502517291f734f8ff7d11
zen811 <khatriharshit84@gmail.com>
2025-10-04 23:41:02 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+ 
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
