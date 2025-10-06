d2c23e9165c09ba18440c7e9af4ef3b0bc04a3b0
zen811 <khatriharshit84@gmail.com>
2025-10-06 22:47:16 +0530

    removed unused imports

--- programs/data_analysis_agent.py
+++ programs/data_analysis_agent.py
@@ -1,9 +1,4 @@
 
-
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import seaborn as sns
 import xgboost as xgb 
 
  
--- programs/main.py
+++ programs/main.py
@@ -1,7 +1,3 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
-
  
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
